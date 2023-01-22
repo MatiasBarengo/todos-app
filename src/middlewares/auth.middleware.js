4b2859ff7061d125792adfdc489b6dbc6c1d8308
Matias Barengo <matiasbarengo99@gmail.com>
2023-01-22 15:02:26 -0300

    actualizacion token

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,27 +4,36 @@ require( 'dotenv' ).config();
 const authMiddleware = ( req, res, next ) =>
 {
   let { authorization: token } = req.headers;
-  token = token.replace( 'Bearer ', '' );
+  token = token?.replace( 'Bearer ', '' );
   console.log( token );
-  jwt.verify(
-    token,
-    process.env.JWT_SECRET,
-    { algorithms: 'HS512' },
-    ( err, decoded ) =>
-    {
-      if ( err )
+  if ( token )
+  {
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET,
+      { algorithms: 'HS512' },
+      ( err, decoded ) =>
       {
-        res.status( 400 ).json( {
-          error: 'invalid token',
-          message: 'token invalido o expirado'
-        } );
-      } else
-      {
-        console.log( decoded );
-        next();
+        if ( err )
+        {
+          res.status( 400 ).json( {
+            error: 'invalid token',
+            message: 'token invalido o expirado'
+          } );
+        } else
+        {
+          console.log( decoded );
+          next();
+        }
       }
-    }
-  )
+    )
+  } else
+  {
+    res.status( 400 ).json( {
+      error: "no token provided",
+      message: "no se esta enviando un token de autenticacion"
+    } )
+  }
 
 };
 
